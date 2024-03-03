07c0de66ea182f26585380e9d0df8c12b40df943
Felix Victor <felix.victor.na@gmail.com>
2024-03-03 10:13:11 +0100

    build: updated build process

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,16 +1,16 @@
 import type { UserConfig } from "@commitlint/types"
 
 const config: UserConfig = {
-  extends: ["@commitlint/config-conventional"],
-  ignores: [(message) => message.includes("WIP")],
-  rules: {
-    "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
-    "type-enum": [
-      2,
-      "always",
-      ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
-    ],
-  },
+    extends: ["@commitlint/config-conventional"],
+    ignores: [(message) => message.includes("WIP")],
+    rules: {
+        "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
+        "type-enum": [
+            2,
+            "always",
+            ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
+        ],
+    },
 }
 
 export default config
