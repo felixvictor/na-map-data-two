c417f787ddee81f0a247a69e9f6573213a4c6a60
Felix Victor <felix.victor.na@gmail.com>
2024-03-03 11:23:25 +0100

    build: updated build process

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 /* eslint-env node */
 module.exports = {
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+    ignorePatterns: [".eslintrc.cjs", ".gitignore", "build/", "lib/"],
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
     root: true,
