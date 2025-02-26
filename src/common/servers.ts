8756a30254e648cc9dd5eb500695bf1a84e02067
Felix Victor <felix.victor.na@gmail.com>
2025-02-26 09:58:56 +0100

    feat: war server removed

--- src/common/servers.ts
+++ src/common/servers.ts
@@ -1,15 +1,11 @@
-import type { Server, ServerId, ServerType } from "../@types/server.js"
+import type { Server } from "../@types/server.js"
 
-export const serverIds = ["eu1", "eu2", "eu3"]
+export const serverIds = ["eu2", "eu3"]
 export const testServerIds = ["dev"]
 
-// If changed check also webpack.config
+// https://storage.googleapis.com/nacleanopenworldprodshards/config.txt
 export const servers: Server[] = [
-    { id: "eu1", name: "War", type: "PVP", icon: "war" },
     { id: "eu2", name: "Peace", type: "PVE", icon: "peace" },
     { id: "eu3", name: "Main", type: "PVP", icon: "free" },
     { id: "dev", name: "Test", type: "PVP", icon: "test" },
 ]
-
-export const getServerType = (serverId: ServerId): ServerType =>
-    servers.find((server) => server.id === serverId)?.type ?? "PVP"
