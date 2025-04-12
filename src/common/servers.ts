fbd616d2f5f2b4239a1b873161a45ad66f592d57
Felix Victor <felix.victor.na@gmail.com>
2025-04-12 08:59:09 +0200

    refactor: servers

--- src/common/servers.ts
+++ src/common/servers.ts
@@ -1,9 +1,9 @@
 import type { Server } from "../@types/server.js"
 
-export const serverIds = ["eu2", "eu3"]
+export const serverIds = ["eu3", "eu2"]
 
 // https://storage.googleapis.com/nacleanopenworldprodshards/config.txt
 export const servers: Server[] = [
+    { id: "eu3", name: "Caribbean", type: "PVP", icon: "free" },
     { id: "eu2", name: "Peace", type: "PVE", icon: "peace" },
-    { id: "eu3", name: "Main", type: "PVP", icon: "free" },
 ]
