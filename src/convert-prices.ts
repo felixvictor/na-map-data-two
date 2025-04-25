d9ee8e1a442995a765d38dca13a06db8d5cd43e0
Felix Victor <felix.victor.na@gmail.com>
2025-04-25 13:39:29 +0200

    feat: resource prices

--- src/convert-prices.ts
+++ src/convert-prices.ts
@@ -3,37 +3,25 @@ import type { PriceWood } from "./@types/prices.js"
 import { getApiItems } from "./common/common.js"
 import { defaultPortTax } from "./common/constants.js"
 import { saveJsonAsync } from "./common/file.js"
+import { round } from "./common/format.js"
 import { getCommonPaths } from "./common/path.js"
 import { sortBy } from "./common/sort.js"
 
 let apiItems: APIItemGeneric[]
 
 const getPrices = (): PriceWood[] => {
-    const standardWoods = new Set([
-        26, // Fir
-        39, // Oak
-        44, // Teak
-        300, // Live Oak
-        894, // Mahogany
-        906, // Bermuda Cedar
-        1359, // White Oak
-    ])
-    const rareWoods = new Set([
-        807, // Malabar Teak
-        863, // Rangoon Teak
-        1894, // Danzic Oak
-        1895, // African Oak
-        1898, // New England Fir
-    ])
+    const resourceIds = new Set(
+        apiItems.filter((item) => !item.NotUsed && item.SortingGroup === "Resource.Resources").map((item) => item.Id),
+    )
 
-    return [...standardWoods, ...rareWoods]
-        .map((rareWoodId): PriceWood => {
-            const rareWood = apiItems.find((item) => item.Id === rareWoodId)
+    return [...resourceIds]
+        .map((id): PriceWood => {
+            const item = apiItems.find((item) => item.Id === id)
 
             return {
-                id: rareWoodId,
-                name: rareWood?.Name ?? "",
-                reales: (rareWood?.BasePrice ?? 0) * (1 + defaultPortTax),
+                id,
+                name: item?.Name ?? "",
+                reales: round((item?.BasePrice ?? 0) * (1 + defaultPortTax), 2),
             }
         })
         .sort(sortBy(["id"]))
